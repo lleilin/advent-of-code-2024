2ad455c3ee19a21f3cec5cb3fe6d55ec84ae3937
Lei Lin <leilin919228630@gmail.com>
2024-12-01 23:27:20 -0500

    Update name

---
+++ day-1/day1.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+
+input = open("input.txt", "r")
+# input = open("test.txt", "r")
+
+arr1, arr2 = [],[]
+mp1, mp2 = defaultdict(int), defaultdict(int)
+
+for row in input:
+    v1, v2 = row.split()
+    v1, v2 = int(v1), int(v2)
+    arr1.append(v1)
+    arr2.append(v2)
+    mp1[v1]+=1
+    mp2[v2]+=1
+
+arr1.sort()
+arr2.sort()
+ans1 = 0
+for v1,v2 in zip(arr1, arr2):
+    ans1 += abs(v1-v2)
+print(ans1)
+
+ans2 = 0
+for k,v in mp1.items():
+    tmp = k*v*mp2.get(k, 0)
+    ans2 += tmp
+print(ans2)
